70dad894373792ac2f4dc72bcc5440db938feb26
Irakli Gozalishvili <rfobic@gmail.com>
2011-05-26 16:05:48 +0200

    Updating tests.

--- test/test-graphquire.js
+++ test/test-graphquire.js
@@ -8,46 +8,48 @@
 var path = require('path')
 var getGraph = require('../graphquire').getGraph
 
-function pathFor(name) {
-  return path.join(module.filename, '../fixtures/' + name + '/package.json')
+function optionsFor(name, cachePath) {
+  return {
+    location: path.join(module.filename, '../fixtures/' + name),
+    cachePath: cachePath || "./node_modules"
+  }
 }
 
 exports['test basic'] = function(assert, done) {
-  var filename = pathFor('pckg1')
-  getGraph(filename, function onGraph(error, graph) {
+  var options = optionsFor('pckg1')
+  getGraph(options, function onGraph(error, graph) {
     assert.deepEqual(graph, {
       "name": "pckg1",
-      "cacheURI": "./node_modules",
-      "path": filename,
+      "cachePath": "./node_modules",
+      "location": options.location + '/package.json',
       "modules": {
         "pckg1": {
           "id": "pckg1",
           "path": "./index.js",
           "requirements": {
-            "http!foo.org/a": "http!foo.org/a"
+            "http!foo.org/a": "http!foo.org/a.js"
           }
         },
-        "http!foo.org/a": {
-          "id": "http!foo.org/a",
+        "http!foo.org/a.js": {
+          "id": "http!foo.org/a.js",
           "path": "node_modules/http!foo.org/a.js",
           "uri": "http://foo.org/a.js",
           "requirements": {
-            "./nested/b": "http!foo.org/nested/b"
+            "./nested/b": "http!foo.org/nested/b.js"
           }
         },
-        "http!foo.org/nested/b": {
-          "id": "http!foo.org/nested/b",
+        "http!foo.org/nested/b.js": {
+          "id": "http!foo.org/nested/b.js",
           "path": "node_modules/http!foo.org/nested/b.js",
           "uri": "http://foo.org/nested/b.js",
           "requirements": {
-            "http!bar.org/c": "http!bar.org/c"
+            "http!bar.org/c": "http!bar.org/c.js"
           }
         },
-        "http!bar.org/c": {
-          "id": "http!bar.org/c",
+        "http!bar.org/c.js": {
+          "id": "http!bar.org/c.js",
           "path": "node_modules/http!bar.org/c.js",
           "uri": "http://bar.org/c.js",
-          "requirements": {}
         }
       }
     }, 'correct graph is generated')
